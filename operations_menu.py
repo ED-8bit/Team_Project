ed0a0bf3acbae69d0d29c5bc9208d177dd8f3ed5
kudde44 <nik.shvetsov.07@mail.ru>
2026-04-21 11:19:00 +0300

    Update operations_menu.py

--- operations_menu.py
+++ operations_menu.py
@@ -1,36 +1,37 @@
 import locale
 
-locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+def menu():
+    locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
-choice = 0
+    choice = 0
 
-while True:
-    print("КАЛЬКУЛЯТОР")
-    print("--- МЕНЮ ---")
-    print("1. Сложение")
-    print("2. Вычитание")
-    print("3. Деление")
-    print("4. Умножение")
-    print("5. Выход")
-    print("Ваш выбор: ", end='')
+    while True:
+        print("КАЛЬКУЛЯТОР")
+        print("--- МЕНЮ ---")
+        print("1. Сложение")
+        print("2. Вычитание")
+        print("3. Деление")
+        print("4. Умножение")
+        print("5. Выход")
+        print("Ваш выбор: ", end='')
 
-    choice = int(input())
+        choice = int(input())
 
-    if choice == 1:
-        print("Вы выбрали 1-ю опцию")
-        print()
-    elif choice == 2:
-        print("Вы выбрали 2-ю опцию")
-        print()
-    elif choice == 3:
-        print("Вы выбрали 3-ю опцию")
-        print()
-    elif choice == 4:
-        print("Вы выбрали 3-ю опцию")
-        print()
-    elif choice == 5:
-        print("Выход из программы...")
-        break
-    else:
-        print("Ошибка")
-        print("Выберите один пункт из меню")
+        if choice == 1:
+            print("Вы выбрали 1-ю опцию")
+            print()
+        elif choice == 2:
+            print("Вы выбрали 2-ю опцию")
+            print()
+        elif choice == 3:
+            print("Вы выбрали 3-ю опцию")
+            print()
+        elif choice == 4:
+            print("Вы выбрали 3-ю опцию")
+            print()
+        elif choice == 5:
+            print("Выход из программы...")
+            break
+        else:
+            print("Ошибка")
+            print("Выберите один пункт из меню")
