cd8f0b0e7fe9eb7802b18f00fa03e0a88eb0892c
ahristokan07-lang <ahristokan07@gmail.com>
2026-05-06 16:06:27 +0300

    Сделал функционал калькулятора

---
+++ mult.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    return a * b
+
+
+
+result = multiply(num1, num2)
+
+print(f"{num1} * {num2} = {result}")
---
+++ plus.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+
+result = add(num1, num2)
+
+print(f"{num1} + {num2} = {result}")
