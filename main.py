3872fb14a907aa354be3897a657fe721ffd0b347
ED <edgartozlian@gmail.com>
2026-05-07 09:53:41 +0300

    main.py added

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ def main():
     while True:
         choice = menu()
         
-        if choice == '0':
+        if choice == 0:
             print("Закрытие")
             break
-        if choice in ['1', '2', '3', '4']:
+        if choice in [1, 2, 3, 4]:
             try:
                 a = float(input("Введите первое число: "))
                 b = float(input("Введите второе число: "))
                 
-                if choice == '1':
-                    result = plus(a, b)
-                elif choice == '2':
-                    result = minus(a, b)
-                elif choice == '3':
-                    result = mult(a, b)
-                elif choice == '4':
-                    result = delen(a, b)
+                if choice == 1:
+                    print(add(a, b))
+                elif choice == 2:
+                    print(substract(a, b))
+                elif choice == 3:
+                    print(multiply(a, b))
+                elif choice == 4:
+                    print(divide(a, b))
 
-                print(f"Результат: {result}\n")
+                
             except ValueError:
                 print("Ошибка: введите числа!\n")
         else:
